19ca0ee3499e0f662b1aac9222386fb3183e3ff0
Peterson Umoke <umoke10@hotmail.com>
2019-04-30 19:01:47 +0100

    move changes

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once dirname(__FILE__) . "/vendor/autoload.php";
-
-// the file htat inits it all
-$rest = new SugarRest('http://localhost/suitecrm', 'admin', 'password');
-$all = $rest->select('Accounts', ['name'], 1000, '');
