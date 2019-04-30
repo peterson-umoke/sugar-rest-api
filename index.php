1f4dc1526ff8ebf0aabb14f18d7b5a44f92f613f
Peterson Umoke <umoke10@hotmail.com>
2019-04-30 18:47:56 +0100

    about to push to pakagist

--- index.php
+++ index.php
@@ -3,8 +3,5 @@
 require_once dirname(__FILE__) . "/vendor/autoload.php";
 
 // the file htat inits it all
-$rest = new SugarRest('http://192.168.19.220/realestate', 'admin', 'omnix360');
-$data = $rest->get('Accounts', ['62527b60-3778-a29c-86cd-5cc2ddc8a67d', 'bb254701-e064-c369-f8f6-5cc30283468b'], ['id', 'name']);
-// echo $rest->module_name;
-
-print_r($data);
+$rest = new SugarRest('http://localhost/suitecrm', 'admin', 'password');
+$all = $rest->select('Accounts', ['name'], 1000, '');
