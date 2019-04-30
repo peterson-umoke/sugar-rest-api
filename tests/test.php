ef35f40f031f42669bc96f499a2705d3bc533cb3
Peterson Umoke <umoke10@hotmail.com>
2019-04-30 20:31:31 +0100

    fixed the issue

--- tests/test.php
+++ tests/test.php
@@ -2,7 +2,7 @@
 
 require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php'; // Autoload files using Composer autoload
 
-use Umoke\SugarRest;
+use Umoke\SugarRestLibrary\SugarRest;
 
 $rest = new SugarRest('http://localhost/suitecrm', 'admin', 'password');
-// $rest->example();
+$rest->example();
