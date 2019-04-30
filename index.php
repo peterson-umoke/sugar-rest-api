21b3d936a0ee7a441b0919b426dea07e31a159af
Peterson Umoke <umoke10@hotmail.com>
2019-04-30 15:35:33 +0100

    testing the endpoints

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ require_once dirname(__FILE__) . "/vendor/autoload.php";
 
 // the file htat inits it all
 $rest = new SugarRest('http://192.168.19.220/realestate', 'admin', 'omnix360');
-print_r($rest);
+$data = $rest->get('Accounts', '62527b60-3778-a29c-86cd-5cc2ddc8a67d');
+
+print_r($data);
