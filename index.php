5f64c2127913dbfbacd1616b159e0df64b0648c9
Peterson Umoke <umoke10@hotmail.com>
2019-04-30 11:49:52 +0100

    testing to make sure the class works

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 require_once dirname(__FILE__) . "/vendor/autoload.php";
 
 // the file htat inits it all
-$rest = new SugarRest('http://localhost/suitecrm', 'admin', 'password');
+$rest = new SugarRest('http://192.168.19.220/realestate', 'admin', 'omnix360');
 print_r($rest);
